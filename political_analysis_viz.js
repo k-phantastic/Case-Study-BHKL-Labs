eaa33679cef6ec0ad5aa277593f0c0ff22dfa6d3
Bokyoung Son <boson@ucsd.edu>
2025-11-27 00:26:40 -0800

    Debugging political_analysis_viz.js

--- political_analysis_viz.js
+++ political_analysis_viz.js
@@ -1,7 +1,5 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
-
 export async function initPoliticalAnalysis(containerId) {
     // Load the political analysis data
     const data = await d3.json('data/political_vax_analysis.json');
